b2ae6bf2873c8506156221c6850559dd08a41dd2
Arshia <arshiahsn@gmail.com>
2020-12-14 22:50:20 -0700

    hello world

---
+++ src/components/hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Hello extends React.Component {
+    render () {
+        // TODO: Generates HTML
+        return <div>Hello from hello.js</div>
+    }
+}
+
+export default Hello;
