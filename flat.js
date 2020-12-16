3e623250a55c64b31a8a462f677896a28bf02ea2
Arshia <arshiahsn@gmail.com>
2020-12-15 23:56:19 -0700

    Added more details

--- flat.js
+++ flat.js
@@ -1,12 +1,21 @@
 import React from "react";
+import "./flat.css";
 
 class Flat extends React.Component {
     render () {
         // TODO: Generates HTML
     return (
+        const title = this.props.flat.name + this.props.flat.priceCurrency + "-" + this.props.flat.price;
+
+        const style = {
+            backgroundImage: "url('this.props.flat.imageUrl')"
+        };
+
     <div className="flat">
-        <div className="flat-picture"></div>
-        <div className="flat-title"></div>
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">
+            {this.props.title}
+        </div>
     
     </div>
     );
