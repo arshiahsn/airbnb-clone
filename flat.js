82de481832e3fafabab9f498b2cca59bce2f28fa
Arshia <arshiahsn@gmail.com>
2020-12-15 23:39:59 -0700

    Added flat component

---
+++ flat.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Flat extends React.Component {
+    render () {
+        // TODO: Generates HTML
+    return <div>
+        <div class="flat-image">
+        </div>
+        <div class="flat-info">
+        </div>
+    
+    </div>
+    }
+}
+
+export default Hello;
