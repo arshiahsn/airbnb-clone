046bd0d3e0231628551cdfab6b433e259b08ad1f
Arshia <arshiahsn@gmail.com>
2020-12-19 21:37:26 -0700

    Marker styling

--- src/App.js
+++ src/App.js
@@ -39,10 +39,15 @@ class App extends React.Component {
         }
 
         if(this.state.selectedFlat){
-            center.lat = this.state.selectedFlat.lat,
-            center.lng = this.state.selectedFlat.lng
+            
+            center = {
+                lat: this.state.selectedFlat.lat,
+                lng: this.state.selectedFlat.lng
+            }
+            
         }
-
+        
+        
         const flat = {
             "name":"Some Flat",
             "imageUrl":"https://media.istockphoto.com/photos/beautiful-luxury-home-exterior-at-twilight-picture-id1026205392",
@@ -59,12 +64,12 @@ class App extends React.Component {
                     <div className="search">
                     </div>
                     <div className="flats">
-                        {this.state.flats.map((flat) => { return <Flat key = {flat.name} flat={flat} selectFlat={selectFlat}/>})}
+                        {this.state.flats.map((flat) => { return <Flat key = {flat.name} flat={flat} selectFlat={this.selectFlat}/>})}
                     </div>
                     <div className="map">
                         <GoogleMapsReact
                             defaultCenter = {center}
-                            defaultZoom = {11} >
+                            defaultZoom = {13} >
                             {this.state.flats.map((flat) => { return <Marker key={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price} />})}
                         </GoogleMapsReact>
                     </div>
